9cf6e924eb3037c7a2469c3afcfb43d3855619a9
diaochan <diaochan@seatent.com>
2024-06-16 12:29:34 +0800

    性别选择

--- src/views/journey/template/CollectInfo.vue
+++ src/views/journey/template/CollectInfo.vue
@@ -2,13 +2,32 @@
  * @Author: diaochan
  * @Date: 2024-06-15 15:02:00
  * @LastEditors: diaochan
- * @LastEditTime: 2024-06-16 11:47:33
+ * @LastEditTime: 2024-06-16 12:28:51
  * @Description: 
 -->
 <template>
   <div class="container">
     <div class="left">
-
+      <div class="sexList">
+        <div class="sexItem sexMale" @click="selectSex('male')" :class="activeSex === 'male' ? 'activeSex' : ''">
+          <div class="sexItemContent">
+            <div class="iconBox">
+              <span class="iconfont icon-male sexIcon"></span>
+              <span class="sexTag">male</span>
+            </div>
+            <div class="sexText">我是一名男性</div>
+          </div>
+        </div>
+        <div class="sexItem" @click="selectSex('female')" :class="activeSex === 'female' ? 'activeSex' : ''">
+          <div class="sexItemContent">
+            <div class="iconBox">
+              <span class="iconfont icon-female sexIcon"></span>
+              <span class="sexTag">female</span>
+            </div>
+            <div class="sexText">我是一名女性</div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="right">
       <FaceTracking ref="refFaceTracking" style="margin-bottom: 2rem;" />
@@ -31,10 +50,18 @@ export default {
     FaceTracking,
     CustomButton
   },
+  data(){
+    return {
+      activeSex: 'male'
+    }
+  },
   setup() {
     
   },
   methods: {
+    selectSex(sex){
+      this.activeSex = sex;
+    },
     reTrack(){
       if(this.$refs.refFaceTracking){
         this.$refs.refFaceTracking.reTrack();
@@ -56,6 +83,81 @@ export default {
 }
 .left{
   width: 45%;
+  padding-top: 15rem;
+  padding-left: 6rem;
+}
+.sexList{
+  display: flex;
+}
+.sexItem{
+  width: 8.5rem;
+  height: 7.5rem;
+  position: relative;
+  cursor: pointer;
+}
+.sexItemContent{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 2;
+  padding-left: 1rem;
+}
+.sexMale{
+  margin-right: 5rem;
+}
+.sexIcon{
+  color: #fff;
+}
+.icon-male{
+  font-size: 4rem;
+}
+.icon-female{
+  font-size: 3.8rem;
+}
+.sexTag{
+  color: #fff;
+  font-size: 1rem;
+}
+.iconBox{
+  display: flex;
+  align-items: flex-end;
+  height: 5.2rem;
+}
+.sexText{
+  color: #fff;
+  font-weight: bold;
+  font-size: 1rem;
+}
+.activeSex .sexIcon,
+.activeSex .sexTag,
+.activeSex .sexText{
+  color: #1A36B8;
+}
+.activeSex::before{
+  content: '';
+  width: 70%;
+  height: 100%;
+  border-radius: 1rem;
+  background-color: #fff;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1;
+}
+.activeSex::after{
+  content: '';
+  width: 100%;
+  height: 70%;
+  border-radius: 1rem;
+  background-color: #fff;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  z-index: 1;
 }
 .right{
   width: 55%;
