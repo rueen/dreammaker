dca8525842e78314c0b31071fbd8a02d0f3d6beb
diaochan <diaochan@seatent.com>
2024-06-17 12:01:31 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@
  * @Author: diaochan
  * @Date: 2024-06-15 15:25:41
  * @LastEditors: diaochan
- * @LastEditTime: 2024-06-17 09:57:01
+ * @LastEditTime: 2024-06-17 11:58:48
  * @Description: 
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/dreammaker/'
 })
