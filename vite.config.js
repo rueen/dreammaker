2399da3dfd309d304771b0845f252879828cd99e
diaochan <diaochan@seatent.com>
2024-06-17 10:36:01 +0800

    deploy

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: diaochan
+ * @Date: 2024-06-11 10:18:46
+ * @LastEditors: diaochan
+ * @LastEditTime: 2024-06-17 10:32:13
+ * @Description: 
+ */
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/dreammaker/',
+});
