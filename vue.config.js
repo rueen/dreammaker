0cd7fe65f29cab07e117f5189d586f2799eb9f57
diaochan <diaochan@seatent.com>
2024-06-17 09:53:27 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+/*
+ * @Author: diaochan
+ * @Date: 2024-06-15 15:25:41
+ * @LastEditors: diaochan
+ * @LastEditTime: 2024-06-17 09:51:00
+ * @Description: 
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  base: '/dreammaker/',
 })
