1f92c7c102ed6b08cd1ea354e72b996aa688a956
diaochan <diaochan@seatent.com>
2025-08-23 18:25:38 +0800

    启用HTTPS

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2024-06-15 15:25:41
  * @LastEditors: diaochan
- * @LastEditTime: 2024-07-10 22:18:09
+ * @LastEditTime: 2025-08-23 18:24:58
  * @Description: 
  */
 const { defineConfig } = require('@vue/cli-service')
@@ -10,6 +10,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/dreammaker/',
   devServer: {
+    https: true, // 启用HTTPS
     proxy: {
       '/site': {
         target: 'https://unidt.ontheway168.cn', // 目标服务器地址
